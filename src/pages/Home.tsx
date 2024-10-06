7979f36d1e40c67ef1909a5fd2e327cb2c6a8ca2
JAmalMichael <eshimi090@gmail.com>
2024-10-06 03:18:24 +0100

    animation on page load

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react"
+import Loader from "../components/Loader" //custom loader
+
+
 
 const Home = () => {
+    const [loading, setLoading] = useState(true);
+
+    //simulating api calling
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            await new Promise((resolve) => setTimeout(resolve, 3000));
+            setLoading(false);
+        }
+        fetchData();
+    }, [])
+
+
   return (
-    <div>Home</div>
+    <div>
+        {loading ? ( <Loader />)  //show loader
+        :
+        (
+            <div className="home">
+                <h1>Welcome to Ride Booking</h1>
+            </div>
+        )}
+    </div>
   )
 }
 
